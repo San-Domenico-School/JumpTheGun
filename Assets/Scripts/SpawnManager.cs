d7b4c788f1d07ecf560d4280cdda42ea61a16324
Loganrud <lrudsenske25@sandomenico.org>
2024-01-05 14:41:22 -0800

    SpawnWorkNow

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -13,15 +13,14 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos2 = new Vector3(25, 5, -0.26f);
     private Vector3 spawnPos3 = new Vector3(22, 5, -0.26f);
     private float startDelay = 2;
-    private float repeatRate = 2;
     public bool gameOver { get; private set; }
 
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
-        InvokeRepeating("SpawnTrophy", startDelay, repeatRate);
-        InvokeRepeating("SpawnPoison", startDelay, repeatRate);
+        InvokeRepeating("SpawnObstacle", startDelay, 4);
+        InvokeRepeating("SpawnTrophy", startDelay, 2);
+        InvokeRepeating("SpawnPoison", startDelay, 6);
     }
    
     // Update is called once per frame
@@ -38,6 +37,8 @@ public class SpawnManager : MonoBehaviour
 
     private void SpawnTrophy()
     {
+        float yPos = UnityEngine.Random.Range(2.0f, 6.2f);
+        spawnPos2 = new Vector3(spawnPos2.x, yPos, spawnPos2.z);
         //Does the same as Spawn obstacle, but for trophies.
         int index = UnityEngine.Random.Range(0, trophyPrefab.Length);
         Instantiate(trophyPrefab[index], spawnPos2, trophyPrefab[index].transform.rotation);
@@ -50,6 +51,8 @@ public class SpawnManager : MonoBehaviour
 
     private void SpawnPoison()
     {
+        float yPos = UnityEngine.Random.Range(2.0f, 6.2f);
+        spawnPos3 = new Vector3(spawnPos3.x, yPos, spawnPos3.z);
         //Does the same as Spawn obstacle, but for poisons.
         int index = UnityEngine.Random.Range(0, poisonPrefab.Length);
         Instantiate(poisonPrefab[index], spawnPos3, poisonPrefab[index].transform.rotation);
